d495cb3506ac0b7d1e8d6c279d1d70cc729d2f44
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-10-30 01:30:04 -0400

    Added data base connection
    - se instalo el orm de sequelize
    - se creo la configuracion de la db

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import router from './routes/index.js';
+import db from './config/db.js'
 
 const app = express()
+
+//conectar a la db
+db.authenticate()
+    .then(() => console.log('base de datos conectada'))
+    .catch(error => console.log(error))
 
 //definir puerto
 const port = process.env.PORT || 4000
