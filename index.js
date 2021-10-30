9531ba51f2cf7017ddd66032df29388bf8dbf918
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2021-10-30 00:08:12 -0400

    added all views

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 4000
 //habilitar pug
 app.set('view engine', 'pug')
 
+//obtener año actual
+app.use((req, res, next) => {
+    const year = new Date()
+
+    res.locals.nombresitio = 'Agencia de Viajes'
+    res.locals.actualYear = year.getFullYear()
+    next()
+})
+
 //Definir la carpeta publica
 app.use(express.static('public'))
 
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,31 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('Inicio')
+
+    res.render('Inicio', {
+        pagina: 'Inicio'
+    })
 })
 
 router.get('/nosotros', (req, res) => {
-    res.render('Nosotros')
+
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    })
 })
 
-router.get('/contacto', (req, res) => {
-    res.send('Contacto')
+router.get('/viajes', (req, res) => {
+
+    res.render('viajes', {
+        pagina: 'Viajes'
+    })
+})
+
+router.get('/testimoniales', (req, res) => {
+
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    })
 })
 
 export default router
